9fe9521c53d66586b7e3b47c88d0af1b606f3df2
WanJu Lin <t2roioaui@gmail.com>
2022-10-09 19:50:47 +0800

    init commit

---
+++ week3.py
@@ -0,0 +1,19 @@
+import urllib.request as request
+import json
+src="https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json"
+with request.urlopen(src) as response:
+    data=json.load(response) # 利用 json 模組處理json 資料格式
+print(data)
+
+data_All=data["result"]["results"]
+with open("data.csv","w",encoding="utf-8") as file:
+    for spot in data_All:
+        if int(spot["xpostDate"][0:4]) >= 2015:
+            address=spot["address"]
+            address_split=(address.split("  "))
+            region=address_split[1]
+            longitude=spot["longitude"]
+            latitude=spot["latitude"]
+            photo=spot["file"]
+            photo_split=photo.split("jpg")
+            file.write(spot["stitle"]+","+region[0:3]+","+longitude+","+latitude+","+photo_split[0]+"jpg"+"\n")
